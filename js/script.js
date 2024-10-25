44a7e9374221490961e05ebb65ad3f329b9c4139
mohamedessamfadelelfeshawy <ana459925@gmail.com>
2024-10-25 07:31:25 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const typed = new Typed(".moveLetter",{
+  strings: ["Photographer",'Designer', "Developer" ,'Freelancer'],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay:2000,
+  });
